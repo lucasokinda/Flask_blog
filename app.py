7a3138c26d6a746b6c46815c187fc9898b7f8f4e
lucasokinda <nathlucas@outlook.fr>
2026-02-27 17:12:15 +0100

    mise a jours du fichier app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def home():
+    return "Hello Azure!"
+
+if __name__ == "__main__":
+    app.run()
